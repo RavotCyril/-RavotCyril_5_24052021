5e866a6df535c50847aa43473bbda73b49c4a22b
GAMER <cyril.ravot@hotmail.fr>
2021-06-08 17:28:21 +0200

    Test

---
+++ index.js
@@ -0,0 +1,23 @@
+(async function() {
+        let articles = await getArticles();
+
+        for (let i in articles) {
+                displayArticle(articles[i]);
+                //console.log(articles[i].name);
+        }
+        
+})()
+
+async function getArticles() {
+        try {
+        let res = await fetch('http://localhost:3000/api/furniture');
+        return await res.json();
+        }
+        catch (e) {
+                console.log(e);
+        }
+}
+
+function displayArticle(article) {
+        console.log(article.description);
+}
