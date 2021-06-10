00c3ed8b7680fce6931bebdb62d18fab16a26311
GAMER <cyril.ravot@hotmail.fr>
2021-06-10 09:26:46 +0200

    Test2

--- index.js
+++ index.js
@@ -2,23 +2,9 @@
         let articles = await getArticles();
 
         for (let i in articles) {
-                displayArticle(articles[i]);
-                //console.log(articles[i].name);
+            displayArticle(articles[i]);
+            //console.log(articles[i].name);
         }
-})()
+    }
 
-async function getArticles() {
-        try {
-        let res = await fetch('http://localhost:3000/api/furniture');
-        return await res.json();
-        }
-        catch (e) {
-                console.log(e);
-        }
-}
-
-function displayArticle(article) {
-        console.log(article._id);
-}
-
-
+    document.createElement(main) get element = document.createElement(main[options]);
