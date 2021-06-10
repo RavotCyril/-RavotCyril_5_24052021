852747d679b35486ee33d245905929017f84deb6
GAMER <cyril.ravot@hotmail.fr>
2021-06-10 13:18:49 +0200

    Test5

--- index.js
+++ index.js
@@ -1,11 +1,16 @@
-let Fourniture;
+let fourniture;
+let main;
 
 //APi Demande
 
 const fetchFourniture = async() => {
     fourniture = await fetch("http://localhost:3000/api/furniture").then(res => res.json());
 
-    console.log(fourniture);
-
 };
 fetchFourniture();
+let b = document.body;
+let newMain = document.createElement("Main");
+let newTexte = document.createTextNode("Tableau Fourniture");
+
+newMain.textContent = "Tableau Fourniture crée et insérer en JavaScript";
+newMain.id = "Fourniture";
