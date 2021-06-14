424fd0a51733ef6d8a2983dbf1be6a2c4c73c17d
GAMER <cyril.ravot@hotmail.fr>
2021-06-14 15:33:09 +0200

    Mise en place de la structure de la page Index.  Reste à appeler les tableaux sur la page.

--- index.js
+++ index.js
@@ -1,16 +1,29 @@
+// Variable générale ( toute la page )
+
 let fourniture;
 let main;
 
-//APi Demande
+// Variable - Fonction - > APi Déclaration
 
 const fetchFourniture = async() => {
-    fourniture = await fetch("http://localhost:3000/api/furniture").then(res => res.json());
-
+    fourniture = await fetch("http://localhost:3000/api/furniture").then((res) =>
+        res.json()
+    );
+    console.log(fourniture);
 };
+// Variable -> APi Appel
+
 fetchFourniture();
+console.log(fetchFourniture);
+
+// Variable -> Balise Main Déclaration
+
 let b = document.body;
 let newMain = document.createElement("Main");
 let newTexte = document.createTextNode("Tableau Fourniture");
 
+// Fonction  ->  Balise Main
+
 newMain.textContent = "Tableau Fourniture crée et insérer en JavaScript";
 newMain.id = "Fourniture";
+console.log(newMain);
