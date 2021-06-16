b1597186eeae5dd7b72d744b69be1eb4a4541842
GAMER <cyril.ravot@hotmail.fr>
2021-06-16 14:51:55 +0200

    Page Index.Js  modifié après l'heure de tutorat.

--- index.js
+++ index.js
@@ -6,27 +6,39 @@ let main;
 // Variable -> Balise Main Déclaration
 
 let b = document.body;
+console.log(b);
 let newMain = document.createElement("Main");
 let newTexte = document.createTextNode("Tableau Fourniture");
 
 newMain.textContent = "Tableau Fourniture crée et insérer en JavaScript";
 newMain.id = "Fourniture";
+b.appendChild(newMain);
 
 console.log(newMain);
 
 // Variable - Fonction - > APi Déclaration
+(async function() {
+    const articles = await getArticles();
+    //console.log(articles);
+    for (let i in articles) {
+        console.log(articles[i]);
+        afficherArticle(articles[i]);
+    }
+})();
 
-const fetchFourniture = async() => {
-    await fetch("http://localhost:3000/api/furniture")
-        .then((res) => res.json())
-        .then((fourniture) => console.table(fourniture));
+async function getArticles() {
+    try {
+        let res = await fetch("http://localhost:3000/api/furniture");
+        return await res.json();
+    } catch (error) {
+        alert(error);
+    }
+}
 
-    console.log(fourniture);
-};
-// Variable -> APi Appel
-
-fetchFourniture();
-
-console.log(fetchFourniture);
-
-window.onload = function() {};
+function afficherArticle(article) {
+    let articleTag = document.createElement("article");
+    let titleTag = document.createElement("h2");
+    titleTag.textContent = article.name;
+    articleTag.appendChild(titleTag);
+    newMain.appendChild(articleTag);
+}
