16d1fc4e68246bb002eab1bcdf00561a6bbb8742
GAMER <cyril.ravot@hotmail.fr>
2021-06-16 10:15:57 +0200

    Mise en place de la récupération individuelle des données.

--- index.js
+++ index.js
@@ -3,27 +3,30 @@
 let fourniture;
 let main;
 
-// Variable - Fonction - > APi Déclaration
-
-const fetchFourniture = async() => {
-    fourniture = await fetch("http://localhost:3000/api/furniture").then((res) =>
-        res.json()
-    );
-    console.log(fourniture);
-};
-// Variable -> APi Appel
-
-fetchFourniture();
-console.log(fetchFourniture);
-
 // Variable -> Balise Main Déclaration
 
 let b = document.body;
 let newMain = document.createElement("Main");
 let newTexte = document.createTextNode("Tableau Fourniture");
 
-// Fonction  ->  Balise Main
-
 newMain.textContent = "Tableau Fourniture crée et insérer en JavaScript";
 newMain.id = "Fourniture";
+
 console.log(newMain);
+
+// Variable - Fonction - > APi Déclaration
+
+const fetchFourniture = async() => {
+    await fetch("http://localhost:3000/api/furniture")
+        .then((res) => res.json())
+        .then((fourniture) => console.table(fourniture));
+
+    console.log(fourniture);
+};
+// Variable -> APi Appel
+
+fetchFourniture();
+
+console.log(fetchFourniture);
+
+window.onload = function() {};
